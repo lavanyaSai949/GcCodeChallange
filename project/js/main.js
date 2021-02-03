2176de332656bb59b1ba2eb1b6dc3e8418d23931
lavanyaSai949 <78438749+lavanyaSai949@users.noreply.github.com>
2021-02-03 07:23:12 -0500

    Variable creation
    fetching json data and creating variables.

---
+++ project/js/main.js
@@ -0,0 +1,37 @@
+//fetching json data from external json file using fetch method
+fetch('./json/pizza-toppings.json')
+    .then(response => response.json())
+    .then(data => {
+        // fetched the data from json successfully
+        jsonReponse = data;
+        console.log(jsonReponse);
+    });
+
+//declaring variables
+let jsonReponse = [];
+let search_input = document.getElementById('search'); // search input field
+let toppingListDropdown = document.getElementById('toppingListDropdown'); // dropdown list
+let dropdown = document.getElementById('dropdown'); // dropdown container
+let selectedToppingList = document.querySelector(".selected-toppings-list"); // selected list
+let search_text = document.querySelector(".search-text"); // label field
+let selectedItemArr = [];
+let filteredListArr = [];
+
+//default dropdown should be hidden
+dropdown.style.display = 'none';
+
+
+
+
+
+//append and display selected topping list template
+const displaySelectedOption = (option) => {
+    console.log(option)
+    var resp = jsonReponse.find(x => x.id == option);
+    const item = `
+      <li class="list-items">
+          <span>${resp.name}</span>
+          <span class="delete" id="${resp.id}">×</span>
+        </li>`;
+    selectedToppingList.innerHTML += item;
+};
